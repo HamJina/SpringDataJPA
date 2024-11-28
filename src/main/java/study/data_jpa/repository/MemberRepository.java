f850e476f4ba3c2ec2e3f25e82b4eff7a7eaa2ca
jina ham <hamjina61@gmail.com>
2024-11-28 15:52:10 +0900

    Auditing: 순수JPA

---
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -0,0 +1,41 @@
+package study.data_jpa.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import study.data_jpa.entity.Member;
+
+import org.springframework.data.domain.Pageable;
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRspositoryCuustom {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username= :username and m.age= :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new study.data_jpa.repository.MemberDto(m.id, m.username, t.name) " + "from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
+
+    Page<Member> findByAge(int age, Pageable pageable);
+
+    //카운트 쿼리 분리
+    @Query(value = "select f from Member m",
+    countQuery = "select count(m.username) from Member m")
+    Page<Member> findMemberAllCountBy(Pageable pageable);
+
+    @Modifying
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+}
